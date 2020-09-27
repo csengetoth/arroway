5d29f1ca73eab188b0ff24f80cd3a9188b4b8736
csengetoth <55600773+csengetoth@users.noreply.github.com>
2020-09-27 12:36:32 +0200

    Add files via upload

---
+++ Gagyi_3.java
@@ -0,0 +1,12 @@
+public class Gagyi_3 {
+
+	public static void main(String[] args) {
+	
+		Integer x = -125;
+		Integer t = -125;
+
+		while (x <= t && x >= t && t!=x) {
+			System.out.println("vegtelen");
+		}
+	}
+}
---
+++ Yoda.java
@@ -0,0 +1,19 @@
+public class Yoda {
+
+	public static void main(String[] args) {
+
+		String s = null;
+
+		//NullPointerEx.
+		if(s.equals("yoda")){
+		System.out.println("OK");
+		}
+
+		//Yoda condition
+		/*if("yoda".equals(s)){
+		System.out.println("OK");
+		}*/
+
+	}
+
+}
